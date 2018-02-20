b56352c8106350cc0ec79b7c7ac9d7a9a2439b94
Jackingotn <crdavies@hotmail.co.uk>
2018-02-20 11:59:07 +0000

    Started Random

---
+++ Random.py
@@ -0,0 +1,36 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Craig
+#
+# Created:     25/11/2015
+# Copyright:   (c) Craig 2015
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+import time
+MyFile=open('lyrical.txt','r')
+words={}
+count=0
+
+songout = open('Output.txt','w')
+
+
+from collections import defaultdict
+
+
+def leaders():
+    counts = defaultdict(int)
+    for x in MyFile.read().split():
+        counts[x] += 1
+    return sorted(counts.items(), reverse=True, key=lambda tup: tup[1])
+
+
+start = time.time()
+#print('\n'.join('%s %s' % x for x in leaders()))
+for x in leaders():
+    print (x)
+end = time.time()
+
+print (end - start)
